353e9f9f6be9c22fe866ce38d24802328bf666c6
ktarow <ktarou569@gmail.com>
2015-11-21 13:59:22 +0900

    add: tap

--- roles/osx/recipe.rb
+++ roles/osx/recipe.rb
@@ -24,6 +24,10 @@ BREW_CASK_PACKAGES = [
   'vlc'
 ]
 
+BREW_TAP = [
+  'caskroom/versions'
+]
+
 execute 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"' do
   not_if 'which brew'
 end
@@ -34,10 +38,27 @@ BREW_PACKAGES.each do |pkg|
   end
 end
 
+BREW_TAP.each do |pkg|
+a execute "brew tap" do
+    command <<-EOS
+      brew tap #{pkg}
+    EOS
+  end
+end
+
+execute 'Change a login shell' do
+  command <<-EOS
+    sudo sh -c "echo '/usr/local/bin/zsh' >> /etc/shells"
+    chsh -s /usr/local/bin/zsh
+    echo 'export HOMEBREW_CASK_OPTS="--appdir=/Applications"' > #{ENV['HOME']}/.zshrc   
+  EOS
+
+  not_if 'grep "/usr/local/bin/zsh" /etc/shells'
+end
+
 BREW_CASK_PACKAGES.each do |pkg|
   execute "install cask" do
     command <<-EOS
-      export HOMEBREW_CASK_OPTS="--appdir=/Applications"
       brew cask install #{pkg}
     EOS
   end
@@ -48,15 +69,6 @@ execute 'for alfred' do
     brew cask alfred link
   EOS
   only_if 'test -d /opt/homebrew-cask/Caskroom/alfred'
-end
-
-execute 'Change a login shell' do
-  command <<-EOS
-    sudo sh -c "echo '/usr/local/bin/zsh' >> /etc/shells"
-    chsh -s /usr/local/bin/zsh
-  EOS
-
-  not_if 'grep "/usr/local/bin/zsh" /etc/shells'
 end
 
 git "#{ENV['HOME']}/dotfiles" do
