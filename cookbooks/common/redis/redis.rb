a3f4c195ba4b8501558fa281a67830048a13e7a0
ktarow <ktarou569@gmail.com>
2016-09-14 20:18:54 +0900

    add redis version

--- cookbooks/common/redis/redis.rb
+++ cookbooks/common/redis/redis.rb
@@ -1,10 +1,11 @@
-REDIS_PATH="http://download.redis.io/releases/redis-3.2.3.tar.gz"
+VERSION="3.2.3"
+REDIS_PATH="http://download.redis.io/releases/redis-#{VERSION}.tar.gz"
 
 execute "curl #{REDIS_PATH} -o /tmp/redis.tar.gz"
 execute "tar xzf /tmp/redis.tar.gz -C /opt"
 execute "install" do
   command <<-EOS
-    cd /opt/redis && make && make install
+    cd /opt/redis-#{VERSION} && make && make install
   EOS
   not_if "[ -e /var/run/redis_*.pid ]"
 end
