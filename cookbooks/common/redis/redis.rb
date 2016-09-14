d690ecfd14fe807e6ccfe5e1c38946bb395a5411
ktarow <ktarou569@gmail.com>
2016-09-14 20:17:21 +0900

    modify redis path

--- cookbooks/common/redis/redis.rb
+++ cookbooks/common/redis/redis.rb
@@ -1,7 +1,7 @@
 REDIS_PATH="http://download.redis.io/releases/redis-3.2.3.tar.gz"
 
 execute "curl #{REDIS_PATH} -o /tmp/redis.tar.gz"
-execute "tar xzf /tmp/redis.tar.gz -C /opt/redis"
+execute "tar xzf /tmp/redis.tar.gz -C /opt"
 execute "install" do
   command <<-EOS
     cd /opt/redis && make && make install
