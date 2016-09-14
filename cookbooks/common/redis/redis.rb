ccb64a82cbae372de019f56ff864cbaa266f3b17
ktarow <ktarou569@gmail.com>
2016-09-14 16:23:24 +0900

    add redis

---
+++ cookbooks/common/redis/redis.rb
@@ -0,0 +1,10 @@
+REDIS_PATH="http://download.redis.io/releases/redis-3.2.3.tar.gz"
+
+execute "curl #{REDIS_PATH} -O /tmp/redis.tar.gz"
+execute "tar xzf /tmp/redis.tar.gz -C /opt/redis"
+execute "install" do
+  command <<-EOS
+    cd /opt/redis && make && make install
+  EOS
+  not_if "[ -e /var/run/redis_*.pid ]"
+end
