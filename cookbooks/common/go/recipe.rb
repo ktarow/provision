43a1f761ccaf8917e74ff7816ee0c977bfa5c52b
ktarow <ktarou569@gmail.com>
2016-10-11 18:55:04 +0900

    update golang recipe

--- cookbooks/common/go/recipe.rb
+++ cookbooks/common/go/recipe.rb
@@ -3,13 +3,21 @@ VERSION = '1.6.1'
 FILENAME = "go#{VERSION}.linux-amd64.tar.gz"
 URI = "https://storage.googleapis.com/golang/#{FILENAME}"
 GOROOT = '/opt/go'
+GOPATH = '$HOME/go'
 
-execute "append path" do
+execute "append GOROOT" do
   command <<-EOS
     echo 'export GOROOT=#{GOROOT}' >> /etc/profile.d/go.sh
     echo 'export PATH="$GOROOT/bin:$PATH"' >> /etc/profile.d/go.sh
   EOS
   not_if "[ -e /etc/profile.d/go.sh ]"
+end
+
+execute "append GOPATH" do
+  command <<-EOS
+    echo 'export GOPATH=#{GOPATH}' >> $HOME/.bash_profile
+  EOS
+  not_if "grep 'GOPATH' $HOME/.bash_profile"
 end
 
 execute "curl #{URI} -o /tmp/#{FILENAME}" do
--- roles/centos68/recipe.rb
+++ roles/centos68/recipe.rb
@@ -1,4 +1,5 @@
 include_recipe '../../cookbooks/centos/yum/recipe.rb'
 include_recipe '../../cookbooks/centos/nginx/nginx.rb'
 include_recipe '../../cookbooks/common/python/python3.rb'
+include_recipe '../../cookbooks/common/go/recipe.rb'
 include_recipe '../../cookbooks/common/redis/redis.rb'
